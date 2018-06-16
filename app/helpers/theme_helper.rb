27d3c7da1a6f3c8d39852c020b4dc1f266d47a3b
Walfie <walfington@gmail.com>
2018-06-16 18:34:37 -0400

    Remove unused code and fix default theme color

--- app/helpers/theme_helper.rb
+++ app/helpers/theme_helper.rb
@@ -1,36 +1,23 @@
 # frozen_string_literal: true
 
 module ThemeHelper
-  def stylesheet_for_layout
-    theme_pack = "theme_#{site_theme_for_user}.css"
-
-    if asset_exist? theme_pack
-      theme_pack
-    else
-      'default'
-    end
-  end
-
   def site_theme_for_user
     current_user&.account&.user&.setting_theme || Setting.theme
   rescue
-    Setting.site_theme
+    Setting.theme
   end
 
   THEME_COLORS = {
-    'default' => '#282c37',
+    'default' => '#df57a4',
+    'mastodon' => '#282c37',
+    'contrast' => '#282c37',
+    'mastodon-light' => '#282c37',
     'kkt' => '#df57a4'
   }.freeze
 
   # For Android tab color
   def theme_color_for_user
     THEME_COLORS[site_theme_for_user] || THEME_COLORS['default']
-  end
-
-  def asset_exist?(path)
-    true if Webpacker::Manifest.lookup(path)
-  rescue Webpacker::FileLoader::NotFoundError
-    false
   end
 end
 
